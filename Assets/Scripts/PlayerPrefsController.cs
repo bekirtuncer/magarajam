71ccbd13afdffe3395063617bd10cd9de225c697
Bekir Tuncer <bekirtuncer97@gmail.com>
2021-07-10 15:03:22 +0300

    bekir - müzik

--- Assets/Scripts/PlayerPrefsController.cs
+++ Assets/Scripts/PlayerPrefsController.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class PlayerPrefsController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    const string MASTER_VOLUME_KEY = "master volume";
+    const string DIFFICULTY_KEY = "difficulty";
+
+    const float MIN_VOLUME = 0f;
+    const float MAX_VOLUME = 1f;
+
+    public static void SetMasterVolume(float volume)
     {
-        
+        if(volume >= MIN_VOLUME && volume <= MAX_VOLUME)
+        {
+            Debug.Log("Master volume set to " + volume);
+            PlayerPrefs.SetFloat(MASTER_VOLUME_KEY, volume);
+        }
+        else
+        {
+            Debug.LogError("Master volume is out of range");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public static float GetMasterVolume()
     {
-        
+        return PlayerPrefs.GetFloat(MASTER_VOLUME_KEY);
     }
 }
